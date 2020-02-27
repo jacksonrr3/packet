15689d52aa4dd2e5c90b926f819d2940edf325dd
jacksonrr3 <49640784+jacksonrr3@users.noreply.github.com>
2020-02-27 13:00:15 +0300

    Update packet.h

--- packet.h
+++ packet.h
@@ -2,33 +2,14 @@
 
 #include <iostream>
 #include <string>
+#include <sstream>
 #include <vector>
 #include <array>
 #include <algorithm>
 #include <utility>
 
-//вспомогательная функция
-std::string int_to_hex(char i) {
-	if (i < 10 && i >= 0) { return std::move(std::to_string(i)); }
-	else {
-		if (i == 10) { return "a"; }
-		if (i == 11) { return "b"; }
-		if (i == 12) { return "c"; }
-		if (i == 13) { return "d"; }
-		if (i == 14) { return "e"; }
-		if (i == 15) { return "f"; }
-	}
-}
-
-//вспомогательная функция
-std::string to_hex(unsigned char c) {
-	char second = c & 0x0F;
-	char first = c >> 4;
-	return std::move(int_to_hex(first) + int_to_hex(second));
-}
 
 class Protocol {
-
 
 public:
 	Protocol() = default;
@@ -55,23 +36,19 @@ public:
 	}
 
 	std::string dstmac() {
-		return std::move(to_hex(dest_mac_[0]) + " " +
-			to_hex(dest_mac_[1]) + " " +
-			to_hex(dest_mac_[2]) + " " +
-			to_hex(dest_mac_[3]) + " " +
-			to_hex(dest_mac_[4]) + " " +
-			to_hex(dest_mac_[5])
-		);
+		std::stringstream ss;
+		ss << std::hex << (int)dest_mac_[0] << " " << (int)dest_mac_[1] << " " <<
+			(int)dest_mac_[2] << " " << (int)dest_mac_[3] << " " <<
+			(int)dest_mac_[4] << " " <<	(int)dest_mac_[5];
+		return std::move(std::string(ss.str()));
 	}
 
 	std::string srcmac() {
-		return std::move(to_hex(source_mac_[0]) + " " +
-			to_hex(source_mac_[1]) + " " +
-			to_hex(source_mac_[2]) + " " +
-			to_hex(source_mac_[3]) + " " +
-			to_hex(source_mac_[4]) + " " +
-			to_hex(source_mac_[5])
-		);
+		std::stringstream ss;
+		ss << std::hex << (int)source_mac_[0] << " " << (int)source_mac_[1] << " " <<
+			(int)source_mac_[2] << " " << (int)source_mac_[3] << " " <<
+			(int)source_mac_[4] << " " << (int)source_mac_[5];
+		return std::move(std::string(ss.str()));
 	}
 
 	std::string scrip() { return ""; }
@@ -112,7 +89,8 @@ public:
 		header_check_sum_ = data[10] * 256 + data[11];
 		std::copy_n(data + 12, 4, src_ip_.begin());
 		std::copy_n(data + 16, 4, dst_ip_.begin());
-		std::copy_n(data + 20, header_size_ - 20, options_.begin());
+		options_.resize((header_size_ * 4) - 20);
+		std::copy_n(data + 20, (header_size_ * 4) - 20, options_.begin());
 	}
 
 	std::string scrip() {
@@ -136,7 +114,7 @@ public:
 
 
 class L4ProtoUDP : public Protocol {
-	std::array<unsigned char, 8> udp_header_;
+	//std::array<unsigned char, 8> udp_header_;
 	int source_port_;
 	int destination_port_;
 	int length_;
@@ -145,13 +123,13 @@ class L4ProtoUDP : public Protocol {
 
 public:
 	L4ProtoUDP(unsigned char* data) {
-		std::copy_n(data, 8, udp_header_.begin());
+	//	std::copy_n(data, 8, udp_header_.begin());
 		source_port_ = data[0] * 256 + data[1];
 		destination_port_ = data[2] * 256 + data[3];
 		length_ = data[4] * 256 + data[5];
 		checksum_ = data[6] * 256 + data[7];
 		data_.resize(length_ - 8);
-		std::copy_n(data+8, length_ - 8, data_.begin());
+		std::copy_n(data + 8, length_ - 8, data_.begin());
 	}
 
 	std::string dstmac() { return ""; }
@@ -161,19 +139,64 @@ public:
 	std::string dstip() { return ""; }
 };
 
+class L4ProtoTCP : public Protocol {
+	//std::array<unsigned char, 8> udp_header_;
+	int source_port_;
+	int destination_port_;
+	unsigned long int sequence_number_;
+	unsigned long int acknowledgment_number_;
+	char offset_;
+	unsigned char reserv_;
+	unsigned char flags_;
+	int window_size_;
+	int checksum_;
+	int urgent_point_;
+	std::vector<unsigned char> options_;  //поле может быть пустым
+	std::vector<unsigned char> data_;   // вектор с данным 
+
+public:
+	L4ProtoTCP(unsigned char* data, std::size_t length) {
+		//std::copy_n(data, 8, udp_header_.begin());
+		source_port_ = data[0] * 256 + data[1];
+		destination_port_ = data[2] * 256 + data[3];
+		sequence_number_ = ((data[4] * 256 + data[5]) * 256 + data[6]) * 256 + data[7];
+		acknowledgment_number_ = ((data[8] * 256 + data[9]) * 256 + data[10]) * 256 + data[11];
+		offset_ = data[12] >> 4;
+		reserv_ = (data[12] & 0b00001111) << 2 + (data[13] & 0b11000000) >> 6;
+		flags_ = data[13] & 0b00111111;
+		window_size_ = data[14] * 256 + data[15];
+		checksum_ = data[16] * 256 + data[17];
+		urgent_point_ = data[18] * 256 + data[19];
+		options_.resize((offset_ * 4) - 20);
+		std::copy_n(data + 20, (offset_ * 4) - 20, options_.begin());
+		data_.resize(length - (offset_ * 4));
+		std::copy_n(data + (offset_ * 4), length - (offset_ * 4), data_.begin());
+	}
+
+	std::string dstmac() { return ""; }
+	std::string srcmac() { return ""; }
+
+	std::string scrip() { return ""; }
+	std::string dstip() { return ""; }
+
+};
+
 
 class Packet {
 
 	std::size_t packet_lenght_;					//длинна пакета
-	unsigned char* packet_ = nullptr;				//указатель на пакет данных, равен указателю на заголовок 2-го уровня
-	unsigned char* l3_ip_ = nullptr;				//указатель на залоговок 3-го уровня
-	std::size_t l3_header_lenght_;					//длинна заголовка 3-го уровня в байтах
-	unsigned char* l4_ = nullptr;					//указатель на залоговок 4-го уровня
+	unsigned char* packet_ = nullptr;			//указатель на пакет данных, равен указателю на заголовок 2-го уровня
+	std::size_t l2_header_lenght_ = 14;			//длинна заголовка 2-го уровня в байтах
+	unsigned char* l3_ip_ = nullptr;			//указатель на залоговок 3-го уровня
+	std::size_t l3_header_lenght_;				//длинна заголовка 3-го уровня в байтах
+	unsigned char* l4_ = nullptr;				//указатель на залоговок 4-го уровня
 	unsigned char l4_type_;						//тип протокола 4-го уровня
 
 	bool l2_chsum_ = true;						//проверка контрольной суммы
 	bool l3_chsum_ = true;						//проверка контрольной суммы
 	bool l4_chsum_ = true;						//проверка контрольной суммы
+
+	//unsigned char* data_ = nullptr;			//указатель на данные. 
 
 	Packet(const Packet& p) = delete;
 	Packet& operator=(const Packet& p) = delete;
@@ -184,12 +207,12 @@ public:
 		packet_lenght_(lenght),
 		packet_(data)
 	{
-		l3_ip_ = packet_ + 14;
-		l3_header_lenght_ = (packet_[14] & 0x0F) * 4;
+		l3_ip_ = packet_ + l2_header_lenght_;
+		l3_header_lenght_ = (packet_[l2_header_lenght_] & 0x0F) * 4;
 		l4_ = l3_ip_ + l3_header_lenght_;
 		l4_type_ = l3_ip_[9];
 
-		//реализовать подсчет контрольных сумм, или подсчитывать кс в методах L2, L3, L4
+		//реализовать подсчет контрольных сумм
 	}
 
 	bool is_packet_correct() {
@@ -207,7 +230,7 @@ public:
 
 	Protocol* l3() {
 		if (l3_chsum_) {
-			return new L3ProtoIPv4(packet_ + 14, l3_header_lenght_);
+			return new L3ProtoIPv4(packet_ + l2_header_lenght_, l3_header_lenght_);
 		}
 		else {
 			return nullptr;
@@ -218,6 +241,9 @@ public:
 		if (l4_chsum_) {
 			if (l4_type_ == 17) {
 				return new L4ProtoUDP(l4_);
+			}
+			if (l4_type_ == 6) {
+				return new L4ProtoTCP(l4_, packet_lenght_ - (l2_header_lenght_ + l3_header_lenght_));
 			}
 		}
 		else {
